4650168e70ed3116fb6f555793aff03658a2a9b0
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2024-12-18 23:02:27 +0000

    Update century_from_year.cpp

--- problem_solving/CodeWars/2024/century_from_year.cpp
+++ problem_solving/CodeWars/2024/century_from_year.cpp
@@ -1,1 +1,23 @@
+#include <cmath>
 
+int centuryFromYear ( int year ) {
+  
+  // 1. Using if Condition :
+  if ( year % 100 == 0 ) {
+    
+    return year / 100 ;
+    
+  }
+  else {
+    
+    return year / 100 + 1 ;
+    
+  }
+  
+  // 2. Using Ternary Condition ;
+  return year % 100 == 0 ? year / 100 : year / 100 + 1;
+
+  // 3. Using Ceil Methode ;
+  return std::ceil( year / 100 ) ;
+
+}
