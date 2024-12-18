9b69f314e315ec8647ca163d1167aceba528ed4e
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2024-12-18 23:31:16 +0000

    Update n_divisible_by_x_and_y.cpp

--- problem_solving/CodeWars/2024/n_divisible_by_x_and_y.cpp
+++ problem_solving/CodeWars/2024/n_divisible_by_x_and_y.cpp
@@ -1,1 +1,13 @@
+bool isDivisible(int n, int x, int y) {
 
+  // 1. Using if Condiction
+  if ( n % x == 0 && n % y == 0 ) {
+    return true ;
+  }
+  else {
+    return false ;
+  }
+
+  // 2. Using Ternary Conditional
+  return n % x == 0 && n % y == 0 ? true : false ;
+}
