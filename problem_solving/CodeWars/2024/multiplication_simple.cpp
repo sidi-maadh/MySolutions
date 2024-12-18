4a862fa2c4ae63e8b09cd8a867f095d1b0caf755
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2024-12-18 22:20:31 +0000

    Update multiplication_simple.cpp

--- problem_solving/CodeWars/2024/multiplication_simple.cpp
+++ problem_solving/CodeWars/2024/multiplication_simple.cpp
@@ -1,1 +1,14 @@
-
+function simpleMultiplication(number) {
+  
+  // 1. Using if Condition :
+  if (number %2 == 0){
+      return number * 8;
+    }
+  else {
+    return number * 9;
+  }
+  
+  // 2. Using Ternary Condition ;
+  return number %2 == 0 ? number * 8 : number * 9;
+  
+}
