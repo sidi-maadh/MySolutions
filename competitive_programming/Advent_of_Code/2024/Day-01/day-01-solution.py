a99d9e8560f758716b0d3b24f6445eb98f464b31
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2024-12-19 16:27:23 +0000

    Update day-01-solution.py

--- competitive_programming/Advent_of_Code/2024/Day-01/day-01-solution.py
+++ competitive_programming/Advent_of_Code/2024/Day-01/day-01-solution.py
@@ -26,8 +26,36 @@ def part1():
 
     print(total)
 
-part1()
-
 
 # --- Part Two ---
+def part2():
+    lines = []
 
+    with open("input.txt") as file:
+        lines = file.readlines()
+
+    location_list = []
+    frequency_map = {}
+
+    for line in lines:
+        line = line.strip()  # Enlever les espaces et les retours à la ligne
+        if line:  # Si la ligne n'est pas vide
+            parts = line.split(' ')
+            location_list.append(int(re.sub(r'\D', '', parts[0])))  # Garder uniquement les chiffres
+
+            frequency_key = int(re.sub(r'\D', '', parts[-1]))
+            frequency_map[frequency_key] = frequency_map.get(frequency_key, 0) + 1
+
+
+    similarity_score = 0
+    for i in range(len(location_list)):
+        if frequency_map.get(location_list[i], 0) != 0:
+            similarity_score += location_list[i] * frequency_map[location_list[i]]
+
+    print(similarity_score)
+
+
+if __name__ == "__main__":
+
+    part1() 
+    part2()
