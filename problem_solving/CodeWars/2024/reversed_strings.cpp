355dd01551aa3332c455f7d67cc3896708a111ac
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2024-12-19 00:14:44 +0000

    Update reversed_strings.cpp

--- problem_solving/CodeWars/2024/reversed_strings.cpp
+++ problem_solving/CodeWars/2024/reversed_strings.cpp
@@ -1,1 +1,16 @@
+#include <string>
+using namespace std ; 
 
+string reverseString (string str )
+{
+  string reversed = "" ;
+  
+  for ( int i = str.length() - 1; i >= 0; i-- ) 
+  {
+    
+    reversed += str[i] ;
+    
+  }
+    
+  return reversed ;
+}
