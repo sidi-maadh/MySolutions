7feaa3e8da9b7570abe4a3713d939b5ba1ec2a57
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2024-12-21 15:00:13 +0000

    Create weird_algorithm.cpp

---
+++ problem_solving/CSES-Problem-Set/2024/weird_algorithm.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    long long n ;
+
+    cin >> n ;
+
+    while (true) {
+        
+        cout << n ; 
+        if (n == 1) break; 
+        cout << " "; 
+
+        
+        if (n % 2 == 0) {
+            n /= 2; 
+        } else {
+            n = n * 3 + 1; 
+        }
+    }
+
+    return 0;
+}
