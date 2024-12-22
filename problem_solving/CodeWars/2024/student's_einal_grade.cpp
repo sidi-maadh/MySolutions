803b480bf902b341417fd346b760a4e8be6b4c94
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2024-12-22 01:24:27 +0000

    Create student's_einal_grade.cpp

---
+++ problem_solving/CodeWars/2024/student's_einal_grade.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int final_grade(int exam, int projects) {
+  
+    return exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : 
+    exam > 50 && projects >= 2 ? 75 : 0; 
+  
+}
