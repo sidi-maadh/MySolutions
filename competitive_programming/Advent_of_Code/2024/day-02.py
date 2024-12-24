34a35b9cd46d2e5af7720e2e0fe7d48f9f969603
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2024-12-24 15:36:27 +0000

    Update day-02.py

--- competitive_programming/Advent_of_Code/2024/day-02.py
+++ competitive_programming/Advent_of_Code/2024/day-02.py
@@ -1,1 +1,60 @@
 
+def is_safe(nums):
+    for i in range(len(nums) - 1):
+        a, b = nums[i], nums[i + 1]
+
+        if not 1 <= abs(a - b) <= 3:
+            return False
+
+        if i == len( nums ) - 2:
+            continue
+
+        c = nums[i + 2]
+        if not a < b < c and not a > b > c:
+            return False
+
+    return True
+
+# --- Part One ---
+def part1():
+    reports = []
+
+    with open("input.txt") as file:
+        reports = file.readlines()
+
+    safe_counter = 0
+    for report in reports:
+        numbers = list(map(int, report.split()))
+        if is_safe(numbers):
+            safe_counter += 1
+
+    return safe_counter
+
+
+
+# --- Part Two ---
+def part2():
+    reports = []
+
+    with open("input.txt") as file:
+        reports = file.readlines()
+
+    safe_counter = 0
+    for report in reports:
+        numbers = list(map(int, report.split()))
+        if is_safe(numbers):
+            safe_counter += 1
+        else:
+            for i in range( len(numbers) ):
+                if is_safe(numbers[:i]  + numbers[i+1:]):
+                    safe_counter += 1
+                    break
+
+    return safe_counter
+
+
+if __name__ == "__main__":
+
+    print(f"Part One: {part1()}")
+    print(f"Part Two: {part2()}")
+
