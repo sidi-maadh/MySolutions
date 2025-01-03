d729f13f66ea3f1b9ae7b09a4d4ff874357d4c0a
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2025-01-03 20:06:08 +0000

    Update drink_about.cpp

--- problem_solving/CodeWars/2025/drink_about.cpp
+++ problem_solving/CodeWars/2025/drink_about.cpp
@@ -1,1 +1,7 @@
+#include <string>
 
+std::string people_with_age_drink(int age) {
+  
+  return age < 14 ? "drink toddy" : age < 18 ? "drink coke" : age < 21 ? "drink beer" : "drink whisky";
+    
+}
