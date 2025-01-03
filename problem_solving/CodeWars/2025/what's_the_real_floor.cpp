69ed752c48ea3e7274b766b6ce7419d59a4673d4
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2025-01-03 11:09:34 +0000

    Update what's_the_real_floor.cpp

--- problem_solving/CodeWars/2025/what's_the_real_floor.cpp
+++ problem_solving/CodeWars/2025/what's_the_real_floor.cpp
@@ -2,5 +2,17 @@ int getRealFloor(int f) {
   
   int floor = 0;
   
-  return f <= 0 ? floor = f : f == 15 ? floor = 13 : floor = f - 1; 
+  if ( f <= 0 ) {
+    floor = f;
+  }
+  else if ( f < 13 ) {
+    floor = f - 1;
+  }
+  else {
+    floor = f - 2;
+  }
+  
+  return floor ;
+  
+//   return f <= 0 ? floor = f : f == 15 ? floor = 13 : floor = f - 1; 
 }
