11980f4fd786e253167f9720df47133c766026fe
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2025-01-04 15:39:50 +0000

    Update abbreviate_a_two_word_name.cpp

--- problem_solving/CodeWars/2025/abbreviate_a_two_word_name.cpp
+++ problem_solving/CodeWars/2025/abbreviate_a_two_word_name.cpp
@@ -1,1 +1,13 @@
+#include <string>
 
+std::string abbrevName(std::string name)
+{
+  std::string result = "";
+  
+  result = toupper( name[0] );
+  result += ".";
+  result += toupper( name[ name.find(" ") + 1 ] );
+  
+  
+  return result;
+}
