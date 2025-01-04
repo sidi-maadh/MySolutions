367424c32b5f32dd6febe30bcb147f9fb0a70d9e
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2025-01-04 16:09:26 +0000

    Create you_only_need_one-beginner.cpp

---
+++ problem_solving/CodeWars/2025/you_only_need_one-beginner.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <string>
+
+bool check(const std::vector<std::string>& seq, const std::string& elem) {
+    
+  for ( auto i: seq )
+  {
+    if ( i == elem ) {
+      return true;
+    }
+  }
+  
+  return false;
+}
