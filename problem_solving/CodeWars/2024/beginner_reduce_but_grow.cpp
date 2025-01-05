9fcd45c6c82e0e56ee1e2619605da677db3621e2
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2025-01-05 18:17:06 +0000

    Update beginner_reduce_but_grow.cpp

--- problem_solving/CodeWars/2024/beginner_reduce_but_grow.cpp
+++ problem_solving/CodeWars/2024/beginner_reduce_but_grow.cpp
@@ -1,12 +1,24 @@
 #include <vector>
-int grow(std::vector<int> nums) {
+#include <algorithm>
+
+using namespace std;
+
+int sumOfDifferences(vector<int> arr) {
+    int result = 0;
   
-  int result = 1;
-  for (int i = 0; i < nums.size(); i++)
-  {
-    result *= nums[i];
-  }
-  
-  return result;
+    sort( arr.rbegin(), arr.rend() );
     
+    if ( arr.size() <= 1 )
+    {
+      result = 0;
+    }
+    else 
+    {
+      for (int i = 0; i < arr.size() - 1; i++) 
+      {
+        result += arr[i] - arr[i + 1];
+      }
+    }
+    
+    return result;
 }
