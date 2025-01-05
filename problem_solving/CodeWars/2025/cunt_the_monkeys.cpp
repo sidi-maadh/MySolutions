1ad02830c72b40dbbce23ef86c07348b2206f288
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2025-01-05 20:44:15 +0000

    Update cunt_the_monkeys.cpp

--- problem_solving/CodeWars/2025/cunt_the_monkeys.cpp
+++ problem_solving/CodeWars/2025/cunt_the_monkeys.cpp
@@ -1,1 +1,13 @@
+#include <vector>
 
+std::vector<int> MonkeyCount(int n) {
+
+  std::vector<int> result;
+  
+  for ( int i = 1; i <= n; i++ )
+  {
+    result.push_back(i);
+  }
+  
+  return result;
+}
