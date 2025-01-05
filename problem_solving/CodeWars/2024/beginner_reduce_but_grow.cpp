6e059f40930d71c69443d1869c3d1092b6fd6792
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2025-01-05 18:25:24 +0000

    Update beginner_reduce_but_grow.cpp

--- problem_solving/CodeWars/2024/beginner_reduce_but_grow.cpp
+++ problem_solving/CodeWars/2024/beginner_reduce_but_grow.cpp
@@ -1,1 +1,12 @@
-
+#include <vector>
+int grow(std::vector<int> nums) {
+  
+  int result = 1;
+  for (int i = 0; i < nums.size(); i++)
+  {
+    result *= nums[i];
+  }
+  
+  return result;
+    
+}
